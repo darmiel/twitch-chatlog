cd450464915c6896f4a35335771ca3f61f8e6f5d
Daniel <71837281+darmiel@users.noreply.github.com>
2021-08-04 23:09:19 +0200

    Update stats.sql

--- stats.sql
+++ stats.sql
@@ -17,3 +17,30 @@ FROM users u
          LEFT JOIN messages m ON u.id = m.author_id
 GROUP BY u.id
 ORDER BY COUNT(m.*) DESC;
+
+-- Get deleted messages
+SELECT u.name, m.body, u2.name, m.date
+FROM messages m
+         INNER JOIN users u ON u.id = m.author_id
+         INNER JOIN users u2 ON u2.id = m.channel_id
+WHERE m.deleted IS NOT NULL;
+
+-- Most censored/toxic chat
+WITH b AS (
+    WITH a AS (
+        SELECT m.channel_id, COUNT(m.*) AS del_count
+        FROM messages m
+                 INNER JOIN users c ON c.id = m.channel_id
+        WHERE m.deleted IS NOT NULL
+        GROUP BY m.channel_id
+        ORDER BY COUNT(m.*) DESC
+    )
+    SELECT c.name,
+           a.del_count,
+           (SELECT COUNT(*) FROM messages m WHERE m.channel_id = a.channel_id) AS all_count
+    FROM a
+             INNER JOIN users c ON c.id = a.channel_id
+)
+SELECT b.*, (float8(b.del_count) / float8(b.all_count)) * 100 AS del_idx
+FROM b
+ORDER BY del_idx DESC;
